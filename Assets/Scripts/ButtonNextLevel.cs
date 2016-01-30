563eb84840441d507a16023532f3aaab48ddc5b4
Nicholas <king-devlin@hotmail.com>
2016-01-31 00:10:15 +0100

    MAIN MENU

---
+++ Assets/Scripts/ButtonNextLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonNextLevel : MonoBehaviour
+{
+    public void NextLevelButton(int index)
+    {
+        Application.LoadLevel(index);
+    }
+}
