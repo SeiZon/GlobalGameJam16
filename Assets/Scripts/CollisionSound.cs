2dcd367048cb2d5b876a1fcd2ee19a25faa34509
Kevin Broløs <kevin.broloes@bestbrains.dk>
2016-01-31 14:49:30 +0100

    Cake sounds

--- Assets/Scripts/CollisionSound.cs
+++ Assets/Scripts/CollisionSound.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class CollisionSound : MonoBehaviour {
 	AudioSource audioSource;
+	public bool playOnce = false;
+
+	private bool hasPlayed = false;
 
 	void Start() {
 		audioSource = GetComponentInParent<AudioSource>();
@@ -10,8 +13,12 @@ public class CollisionSound : MonoBehaviour {
 
 	void OnCollisionEnter(Collision collision) {
 		if (collision.relativeVelocity.magnitude > 2) {
-			if(audioSource) {
+			if(audioSource && !hasPlayed) {
 				audioSource.Play();
+
+				if(playOnce) {
+					hasPlayed = true;
+				}
 			}
 		}
 	}
